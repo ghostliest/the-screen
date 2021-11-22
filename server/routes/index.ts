fe2b2150d7f3a6ed5cee9452019089a9581f9ced
LEv0nz <LukEv0nz@gmail.com>
2021-11-22 14:02:23 +0300

    BaseController
    Methods: Search(film, director, actor, country, genre), Create(director, actor)

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,7 +1,13 @@
 const Router = require('express');
 const router = new Router();
 import filmRouter from './filmRouter'
+import userRouter from './userRouter'
+import BaseController from '../controllers/BaseController';
+const baseController = new BaseController();
 
-router.use('/film', filmRouter)
+router.use('/film', filmRouter);
+router.use('/user', userRouter);
+router.get('/search/:name(director|actor|country|genre)?', baseController.search);
+router.post('/create/:name(director|actor)?', baseController.create);
 
 export default router;
