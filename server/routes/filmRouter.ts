698509683d2aafed4e4d71dc8c488082f8d89d56
LEv0nz <LukEv0nz@gmail.com>
2021-11-27 01:34:30 +0300

    Film controller

--- server/routes/filmRouter.ts
+++ server/routes/filmRouter.ts
@@ -1,9 +1,14 @@
 const Router = require('express');
 import FilmController from '../controllers/FilmController';
+import checkRoleMiddleware from '../middleware/checkRoleMiddleware';
 const router = new Router();
 
-const filmController = new FilmController();
+router.post('/', checkRoleMiddleware("ADMIN"), FilmController.create);
+router.put('/:id', checkRoleMiddleware("ADMIN"), FilmController.update);
+router.get('/', FilmController.getAll);
+router.get('/:id', FilmController.getOne);
 
-router.post('/', filmController.create);
+router.post('/upcoming', checkRoleMiddleware("ADMIN"), FilmController.addUpcoming);
+router.delete('/upcoming/delete', checkRoleMiddleware("ADMIN"), FilmController.deleteUpcoming);
 
 export default router;
