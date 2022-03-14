c8faee795cfd1c3091175788694c8da30f197cdb
LEv0nz <LukEv0nz@gmail.com>
2022-03-14 12:29:56 +0300

    Provide store

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { store } from './store/store'
+import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
+)
