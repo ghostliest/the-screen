cbf794647aeb6495717f7504e593218437671019
LEv0nz <LukEv0nz@gmail.com>
2022-03-15 14:40:00 +0300

    Routes constants

---
+++ client/src/utils/consts.ts
@@ -0,0 +1,16 @@
+export const HOME_ROUTE = '/'
+
+export const LOGIN_ROUTE = '/login'
+export const REGISTRATION_ROUTE = '/registration'
+export const USER_ROUTE = '/user'
+export const USER_UPDATE_ROUTE = USER_ROUTE + '/update'
+
+export const FILM_ROUTE = '/film'
+export const PERSON_ROUTE = '/person'
+export const SEARCH_ROUTE = '/search'
+export const ADMIN_ROUTE = '/admin'
+
+export const FOLDERS_ROUTE = '/folders'
+export const FOLDERS_WATCHLATER_ROUTE = `${FOLDERS_ROUTE}?type=watchLater`
+export const FOLDERS_FAVORITE_ROUTE = `${FOLDERS_ROUTE}?type=favorite`
+export const FOLDERS_VIEWED_ROUTE = `${FOLDERS_ROUTE}?type=viewed`
