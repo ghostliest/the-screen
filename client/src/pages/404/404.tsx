7321c4477c4790c4795362ada1f0fcac6aeceba2
LEv0nz <LukEv0nz@gmail.com>
2022-05-02 12:48:02 +0300

    Page 404

---
+++ client/src/pages/404/404.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { InDevelopment } from '..'
+
+export const Error404 = () => (
+	<InDevelopment
+		header='Error 404'
+		gifferIds={['IWWP', '35TJ', 'VNpl']}
+	/>
+)
