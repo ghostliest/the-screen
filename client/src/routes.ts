2ee7625f592dd39c1085d17c49e0311440f362d4
LEv0nz <LukEv0nz@gmail.com>
2022-05-02 12:51:31 +0300

    Routing

---
+++ client/src/routes.ts
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Admin, Auth, Content, Person, Home, FoldersMain, Folder } from './pages'
+import {
+	ADMIN_ROUTE,
+	HOME_ROUTE,
+	FILM_ROUTE,
+	LOGIN_ROUTE,
+	REGISTRATION_ROUTE,
+	PERSON_ROUTE,
+	FOLDERS_ROUTE
+} from './utils/consts'
+
+interface Routes {
+	path: string;
+	Component: () => React.ReactElement
+}
+
+export const adminRoutes: Routes[] = [
+	{	path: ADMIN_ROUTE, Component: Admin }
+]
+
+export const privateRoutes: Routes[] = [
+	{ path: FOLDERS_ROUTE, Component: FoldersMain },
+	{ path: FOLDERS_ROUTE + '/:type', Component: Folder }
+]
+
+export const publicRoutes: Routes[] = [
+	{ path: HOME_ROUTE, Component: Home },
+	{ path: LOGIN_ROUTE, Component: Auth },
+	{	path: REGISTRATION_ROUTE,	Component: Auth },
+	{	path: FILM_ROUTE + '/:id', Component: Content	},
+	{ path: PERSON_ROUTE + '/:id', Component: Person }
+]
