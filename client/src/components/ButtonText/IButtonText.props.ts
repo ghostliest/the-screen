151081d52b66ec5fc301ba9c88d6cfc5e96a28b7
LEv0nz <LukEv0nz@gmail.com>
2022-05-03 12:26:48 +0300

    Added new styles and behaviors

--- client/src/components/ButtonText/IButtonText.props.ts
+++ client/src/components/ButtonText/IButtonText.props.ts
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export interface IButtonText extends React.HTMLAttributes<HTMLButtonElement> {
-	type: 'PRIMARY' | 'SECONDARY',
+	type: 'PRIMARY' | 'SECONDARY' | 'TRANSPARENT' | 'INACTIVE',
 	placeholder: string,
 	className?: string
 	onClick: React.MouseEventHandler<HTMLButtonElement>
